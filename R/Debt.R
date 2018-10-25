e8bcdef19f412d4bc519301562784a0a2c4fadc0
RobHayward <robohay@gmail.com>
2018-10-25 12:37:05 +0100

    Updated sections and colours.

--- R/Debt.R
+++ R/Debt.R
@@ -8,27 +8,24 @@ tail(da)
 # get rid of spares rows at the end 
 da <- da[1:296, ]
 # Get rid of the strand signs in the data
-da[, 1] <- gsub("+ACI-", "", da[,1], fixed = TRUE)
+#da[, 1] <- gsub("+ACI-", "", da[,1], fixed = TRUE)
 da[, 3] <- gsub("+ACI-", "", da[,3], fixed = TRUE) 
 da[, 5] <- gsub("+AC0-", "", da[,5], fixed = TRUE) 
 da[, 8] <- gsub("+ACI-", "", da[,8], fixed = TRUE) 
-da[da[, 1] == "Korea",]
-
+#da[da[, 1] == "Korea",]
 names(da)
 # Past the series names into one column
 #da$Series <- paste( da[, 3], da[,4], da[,5], da[, 6], da[,7], da[,8])
 da$Series <- paste(da[,5], da[,7])
 da$Series <- gsub("  ", " ", da$Series)
 head(da)
-da[250,]
+tail(da)
 # Now just sort the data and create new names. 
-da <- da[, c(1, 2, 9, 17, 11, 12, 13, 14, 15)]
+da <- da[, c(1, 2, 9, 16, 11, 12, 13, 14, 15)]
 names(da) <- c('Country', 'C-Code', 'D-Code', 'Data', '2017Q1', '2017Q2', '2017Q3', '2017Q4', '2018Q1')
 # One saeries (Q117) is a string rather than a numeric 
 da[, "2017Q1"] <- as.integer(da[, "2017Q1"])
 # remove Korea and Macedonia
-da <- da[da[, 1] != "Korea", ]
-da <- da[da[, 1] != "Macedonia", ]
 head(da, n = 8)
 tail(da)
 str(da)
@@ -45,8 +42,19 @@ dat1 <- spread(dat, 'Data', "Debt")
 # Does not work.  It seems right but the numbers are not all there 
 head(dat1)
 tail(dat1)
-head(dat)
-dat1 <- spread(dat, "Data", "Debt", fill = NA)
-dat[c(421, 422), ]
-dat[da1[, 1] == "Korea", ]
-dat[289:290, ]
+#=======================================================
+require(xtable)
+str(dat1)
+head(dat1)
+head(dat$Data, 10)
+dat1$FXDebt <- dat1[, 5]/dat1[, 3]
+dat1$LTFXDebt <- dat1[, 8]/dat1[, 3]
+dat1$STFXDebt <- dat1[, 10]/dat1[, 3]
+
+Findat <- dat1[dat1[, 2] == "2018Q1", ]
+Sortdata <- Findat[order(-Findat$FXDebt), ]
+head(Sortdata)
+# barplot
+barplot(Sortdata$FXDebt[1:20], names.arg = Sortdata$Country[1:20], main = "Major FX Debt", 
+        ylim = c(0, 1), col = 'DarkGreen', las = 2)
+xtable(Sortdata[1:20, c('Country', 'FXDebt', "LTFXDebt", "STFXDebt")], caption = "IMF debt", digis = 2)
